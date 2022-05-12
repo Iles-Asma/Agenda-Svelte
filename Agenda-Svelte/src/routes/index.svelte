7f92a7f8d957a92fcc0750d6830c0bc9d4975ca0
DFady <djillafady@gmail.com>
2022-05-12 12:21:16 +0200

    début squelette

--- Agenda-Svelte/src/routes/index.svelte
+++ Agenda-Svelte/src/routes/index.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>BIENVENUE SUR TON AGENDA</h1>
+<select id="year">
+    <option value="">Année</option>
+    <option value="d">2022</option>
+    <option value="">2023</option>
+    <option value="">2024</option>
+</select>
+
+<select id="month">
+    <option value="">Mois</option>
+    <option value="">Janvier</option>
+    <option value="">Février</option>
+    <option value="">Mars</option>
+    <option value="">Avril</option>
+    <option value="">Mai</option>
+</select>
+
+
