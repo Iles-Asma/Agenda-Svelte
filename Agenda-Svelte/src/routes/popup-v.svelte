541bf38e2dcca9b7f2abda5573e7160372b09615
Iles-Asma <iles.a@icloud.com>
2022-06-05 20:05:50 +0200

    pop up component

---
+++ Agenda-Svelte/src/routes/popup-v.svelte
@@ -0,0 +1,23 @@
+<script>
+
+</script>
+
+    <div class="popup-v-container">
+        <h2>title</h2>
+        <p>description</p>
+        <p></p>
+    </div>
+
+
+
+
+<style>
+    .popup-v-container{
+        padding: 1rem;
+        border: solid 2px red;
+        width: 500px;
+        height: auto;
+        position: absolute;
+    }
+
+</style>
